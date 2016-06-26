d0af8960f96d349e5b12c26c7d57ebef1bf50170
John Draper <johndraper@icloud.com>
2016-06-26 14:47:53 +1000

    Added moment.js

--- server.js
+++ server.js
@@ -3,27 +3,25 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
 var moment = require('moment');
-var now;
-
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
 	console.log('User connectiod via socket.io!');
 
-	now = moment().local().format('ddd Do MMM YYYY H:m:sZ');
-
 	socket.on('message', function (message) {
-		console.log(now + ' Message recieved: ' + message.text);
-		message.text = now + ': ' + message.text;
+		console.log('Message recieved: ' + message.text);
+
+		message.timestamp = moment().valueOf();
+
 		//io.emmit; will send the message to everyone including the sender
 		io.emit('message', message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to the chat application!'
+		text: 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
 	});
 });
 
